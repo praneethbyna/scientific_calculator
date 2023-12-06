cffaad246a8baf5def94bf9df596f2d1e4671d3e
babavali98 <153024723+babavali98@users.noreply.github.com>
2023-12-05 20:20:00 -0600

    Add files via upload
    added code for functions like calculate the result by parsing the calculator expression

--- calculator.js
+++ calculator.js
@@ -15,3 +15,49 @@ function clearInput() {
     document.getElementById("inputField").value = "";
 }
 
+
+
+function calculateResult() {
+    const inputField = document.getElementById("inputField");
+    const expression = inputField.value;
+    try {
+        const result = parseExpression(expression);
+        inputField.value = result.toString();
+    } catch (error) {
+        inputField.value = "Error";
+    }
+}
+
+function parseExpression(expr) {
+    const tokens = expr.match(/(\d+\.?\d*|\+|\-|\*|\/)/g);
+    if (!tokens) {
+        throw new Error("Invalid Expression");
+    }
+
+    let total = parseFloat(tokens[0]);
+
+    for (let i = 1; i < tokens.length; i += 2) {
+        const operator = tokens[i];
+        const nextNumber = parseFloat(tokens[i + 1]);
+
+        switch (operator) {
+            case '+':
+                total += nextNumber;
+                break;
+            case '-':
+                total -= nextNumber;
+                break;
+            case '*':
+                total *= nextNumber;
+                break;
+            case '/':
+                total /= nextNumber;
+                break;
+            default:
+                throw new Error("Invalid Operator");
+        }
+    }
+    return total;
+}
+
+
